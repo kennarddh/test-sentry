c4d99b75a7c6fec0ea155c84d6b9732fb453444a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-03-02 20:22:28 +0700

    feat: Sentry

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 const App: FC = () => {
+	const [State, SetState] = useState(0)
+
 	const error1 = () => {
 		JSON.parse('{test')
 	}
@@ -9,10 +11,35 @@ const App: FC = () => {
 		throw new Error('Error2')
 	}
 
+	const error3 = () => {
+		return 10 / 0
+	}
+
+	const error4 = () => {
+		error4()
+	}
+
+	const error5 = () => {
+		SetState(1)
+	}
+
+	useEffect(() => {
+		SetState(0)
+	}, [State])
+
+	const error6 = () => {
+		// eslint-disable-next-line @typescript-eslint/no-extra-semi
+		;[].length = 4294967296
+	}
+
 	return (
 		<>
 			<button onClick={() => error1()}>error1</button>
 			<button onClick={() => error2()}>error2</button>
+			<button onClick={() => error3()}>error3</button>
+			<button onClick={() => error4()}>error4</button>
+			<button onClick={() => error5()}>error5</button>
+			<button onClick={() => error6()}>error6e</button>
 		</>
 	)
 }
